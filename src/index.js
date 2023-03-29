b2f86777badca33b89c6e8cc9066e98fb8ab9f70
DenysChebotarov <114431465+DenysChebotarov@users.noreply.github.com>
2023-03-29 10:43:53 +0300

    goit-js-hw-11-styles

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const options = {
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
-    per_page: 3,
+    per_page: 40,
     page: 0,
   },
 };
@@ -39,6 +39,7 @@ async function onSubmit(e) {
   }
   console.log(options.params.q);
   try {
+    Notiflix.Notify.success('Слава Україні');
     clearMarkup();
     const images = await fetchImages(searchQuery);
     loadMoreButton.classList.remove('visually-hidden')
@@ -74,19 +75,19 @@ function renderMarkup({ data }) {
       }) => {
         return `
         <div class="photo-card">
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      <img width= 300px height= 200px src="${webformatURL}" alt="${tags}" loading="lazy" />
       <div class="info">
         <p class="info-item">
-          <b>Likes${likes}</b>
+          <b>Likes: ${likes}</b>
         </p>
         <p class="info-item">
-          <b>Views${views}</b>
+          <b>Views: ${views}</b>
         </p>
         <p class="info-item">
-          <b>Comments${comments}</b>
+          <b>Comments: ${comments}</b>
         </p>
         <p class="info-item">
-          <b>Downloads${downloads}</b>
+          <b>Downloads: ${downloads}</b>
         </p>
       </div>
     </div>`;
